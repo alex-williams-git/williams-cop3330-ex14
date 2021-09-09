7c47cc9a17ee2635a542406c24b2b75a525a2d0c
Alexander Williams <alexander.williams@knights.ucf.edu>
2021-09-09 00:10:20 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,39 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Alexander Williams
+ */
+
+package org.example;
+import java.util.Scanner;
+import java.lang.Math;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("What is the amount? ");
+        double subtotal = scan.nextDouble();
+
+        Scanner newscan = new Scanner(System.in);
+
+        System.out.print("What is the state? ");
+        String state = newscan.nextLine();
+
+        double tax = 0.0;
+        double total = 0.0;
+
+        if(state.equals("WI")){
+            tax = Math.round((subtotal * .055) * 100.0) / 100.0;
+            total = Math.round((subtotal + tax) * 100.0) / 100.0;
+
+            System.out.println("The subtotal is $" + String.format("%.2f", subtotal));
+            System.out.println("The tax is $" + tax);
+            System.out.println("The total is $" + total);
+        }else{
+            System.out.println("The total is $" + String.format("%.2f", subtotal));
+        }
+
+    }
+}
